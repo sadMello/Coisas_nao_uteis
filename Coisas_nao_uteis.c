06010b157357428488e505362ddffcdb4c9c78fe
Mello <84051905+sadMello@users.noreply.github.com>
2023-08-21 22:10:31 -0300

    adicionando segunda opcao

--- Coisas_nao_uteis.c
+++ Coisas_nao_uteis.c
@@ -13,14 +13,27 @@ int impares_N(int n){
     }
 }
 
+void sorteio(int n){
+    srand(time(NULL));
+    int aux = rand()%163;
+    printf("\nNumero sorteado: %d",aux);
+    if(n==aux){
+        printf("\nAEEEE vc acertou! parabens ganhou um gato.... mentira nao tem gato mas voce merece\n");
+    }else{
+        printf("\nnhe nhe nhe tu perdeu, nao ganhou o gato :(\n");
+    }
+}
+
 int main(){
-    int escolha, n;
+    int escolha, n, i=1;
     
     while(1==1){
         system("cls");
-        printf("\n------------------------------\nBEM VINDO\n");
-        printf("MENU - coisas nao uteis:\n------------------------------\n");
+        printf("\n------------------------------------------------------------\nBEM VINDO\t\t\tContador de usos: %d\n",i);
+        i++;
+        printf("MENU - coisas nao uteis:\n------------------------------------------------------------\n");
         printf("\n1 - Todos os numeros impares ate N");
+        printf("\n2 - Mini loteria");
         printf("\n0 - sair (tenho alto util a fazer)\n");
         scanf("%d",&escolha);
         if(escolha==0){
@@ -28,12 +41,22 @@ int main(){
         }
         if(escolha==1){
             system("cls");
-            printf("------------------------------\nTodos os numeros impares ate N\n------------------------------\n");
+            printf("------------------------------------------------------------\nTodos os numeros impares ate N\n------------------------------------------------------------\n");
             printf("Escolha N: ");
             scanf("%d",&n);
             impares_N(n);
             printf("\n");
             system("pause");
         }
+        if(escolha==2){
+            system("cls");
+            printf("------------------------------\nse tu acertar aleatoriamente um numero de 1-163 ganha um gato\n------------------------------\n");
+            printf("Escolha seu chute: ");
+            scanf("%d",&n);
+            sorteio(n);
+            printf("\n");
+            system("pause");
+        }
+        
     }
 }
